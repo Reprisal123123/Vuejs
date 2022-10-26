765f4b833ff8724f487176da563380bda1a91ea7
KYK <kezy@daum.net>
2022-10-26 15:56:21 +0900

    State, Props, v-for 학습 추가

---
+++ src/components/Box.vue
@@ -0,0 +1,35 @@
+<template>
+    <div 
+    :class="['box', color]"
+    :style="{ width: width + 'px', height: height + 'px'}">Box
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            color: { type: String, default: "" }
+        },
+        data() {
+            return {
+                width: 40, // 넓이
+                height: 80 // 높이
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    .box {
+        border: 1px solid #000;
+    }
+    .blue {
+        background: #009bff;
+    }
+    .purple {
+        background: #8f46ff;
+    }
+    .green {
+        background: #00bcac;
+    }
+</style>
